72cdb7d79186738cc84fa7714295ff3cadb8fe7e
ultraeon <robloxrulesmariorules@gmail.com>
2025-02-26 13:21:12 -0600

    Commented out vec2 class

--- src/game/Vec2.java
+++ src/game/Vec2.java
@@ -1,3 +1,4 @@
+// class for a vector of length 2 -> <x, y>
 public class Vec2 {
     int x;
     int y;
@@ -5,21 +6,27 @@ public class Vec2 {
         this.x = x;
         this.y = y;
     }
+    // prints as vector
     public String toString() {
-        return "("+x+", "+y+")";
+        return "<"+x+", "+y+">";
     }
+    // elementwise addition
     public Vec2 add(Vec2 other) {
         return new Vec2(x+other.x, y+other.y);
     }
+    // elementwise subtraction
     public Vec2 subtract(Vec2 other) {
         return new Vec2(x-other.x, y-other.y);
     }
+    // scalar multiplication a*<x, y> = <ax, ay>
     public Vec2 sMultiply(int m) {
         return new Vec2(x*m, y*m);
     }
+    // hadamard multiplication <a, b>*<c, d> = <ac, bd>
     public Vec2 hMultiply(Vec2 other) {
         return new Vec2(x*other.x, y*other.y);
     }
+    // dot product calculation <a, b>*<c, d> = ac+bd
     public int dMultiply(Vec2 other) {
         return x*other.x+y*other.y;
     }
