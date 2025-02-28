4d1fea5bde542e8de8460f9b57aa93ec8cb8c08a
ultraeon <robloxrulesmariorules@gmail.com>
2025-02-28 08:44:07 -0600

    Object loading main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         GameHandler game = new GameHandler();
-        game.addObject(new GameObject(new Vec2(5000, 3000), new Vec2(30000, 3000)));
-        GameObject o = new GameObject(new Vec2(1000, 3000), new Vec2(4000, 3000));
-        o.isDeathPlane = true;
-        game.addObject(o);
         DisplayHandler d = new DisplayHandler(game, 60, 30);
-        for(int i = 0; i < 100; i++) {
-            System.out.println(d);
-            Thread.sleep(100);
-            game.doTick(new boolean[]{true, false, false});
+        int[][] test = new int[30][5];
+        for(int i = 0; i < 30; i++) {
+            test[i][0] = (int)(Math.random()*100000);
+            test[i][1] = (int)(Math.random()*4000);
+            test[i][2] = (int)(Math.random()*2000)+1000;
+            test[i][3] = (int)(Math.random()*2000)+1000;
+            test[i][4] = 3;
         }
-        for(int i = 0; i < 200; i++) {
+        game.loadObjects(test);
+        for(int i = 0; i < 1000; i++) {
             System.out.println(d);
             Thread.sleep(100);
-            game.doTick(new boolean[]{false, false, false});
+            game.doTick(new boolean[]{true, false, true});
         }
     }
 }
