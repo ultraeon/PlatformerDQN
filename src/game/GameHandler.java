308d9a7a9aaa8d76848245bc18bbb1d07eba63f0
ultraeon <robloxrulesmariorules@gmail.com>
2025-02-28 08:46:44 -0600

    Object loading

--- src/game/GameHandler.java
+++ src/game/GameHandler.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+// class to handle internals of the game
 public class GameHandler {
     private ArrayList<GameObject> gameObjList;
     private Player player;
@@ -7,9 +8,24 @@ public class GameHandler {
         player = new Player();
         gameObjList = new ArrayList<GameObject>();
         gameObjList.add(new GameObject(new Vec2(-500000, -10000), new Vec2(1000000, 10000)));
+        gameObjList.get(0).isVisible = true;
+        gameObjList.get(0).isTangible = true;
     }
+    // adds an object as is
     public void addObject(GameObject o) {
         gameObjList.add(o);
+    }
+    // adds objects from an int array
+    // each row is an object of form [x, y, width, height, boolean information]
+    public void loadObjects(int[][] objList) {
+        GameObject o;
+        for(int[] obj : objList) {
+            o = new GameObject(new Vec2(obj[0], obj[1]), new Vec2(obj[2], obj[3]));
+            if((obj[4] & 1) == 1) o.isVisible = true;
+            if(((obj[4] >> 1) & 1) == 1) o.isTangible = true;
+            if(((obj[4] >> 2) & 1) == 1) o.isDeathPlane = true;
+            gameObjList.add(o);
+        }
     }
     public String toString() {
         return player.toString();
@@ -19,6 +35,7 @@ public class GameHandler {
     }
     public int getCamCollision(int x, int y) {
         for(GameObject gameObj : gameObjList) {
+            if(!gameObj.isVisible) continue;
             if(gameObj.camCollisionCheck(x, y)) {
                 if(gameObj.isDeathPlane) return 2;
                 return 1;
